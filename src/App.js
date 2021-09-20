6a6c9994b369fad8bd80a253ce176ea30d703adb
ranjanarpit27 <ranjanarpit27@gmail.com>
2021-09-20 23:52:00 +0530

    pathfinging Visulizer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer.jsx";
+import {Tutorial} from "./PathfindingVisualizer/Tutorial/Tutorial.jsx";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PathfindingVisualizer></PathfindingVisualizer>
+      <div className="PopUp">
+        <Tutorial />
+      </div>
     </div>
+    
   );
 }
 
